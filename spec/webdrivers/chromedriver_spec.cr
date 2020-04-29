02dfbd6c470a982840358d209cadaf5bc98d9e16
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2020-04-29 07:03:49 -0500

    remove hard coded specs

--- spec/webdrivers/chromedriver_spec.cr
+++ spec/webdrivers/chromedriver_spec.cr
@@ -1,57 +1,10 @@
 require "../spec_helper"
 
 describe Webdrivers::Chromedriver do
-  before_each { Webdrivers::Chromedriver.install }
-
-  describe ".browser_version" do
-    it "returns semver of installed browser version" do
-      expected_semver = SemanticVersion.new(
-        major: 81,
-        minor: 0,
-        patch: 129,
-        build: "4044"
-      )
-
-      Webdrivers::Chromedriver.browser_version.should eq(expected_semver)
-    end
-  end
-
-  describe ".driver_version" do
-    it "returns semver of installed chromedriver" do
-      expected_semver = SemanticVersion.new(
-        major: 81,
-        minor: 0,
-        patch: 69,
-        build: "4044"
-      )
-
-      Webdrivers::Chromedriver.driver_version.should eq(expected_semver)
-    end
-  end
-
-  describe ".latest_driver_version" do
-    it "returns semver of latest available driver version for download" do
-      expected_semver = SemanticVersion.new(
-        major: 81,
-        minor: 0,
-        patch: 69,
-        build: "4044"
-      )
-
-      Webdrivers::Chromedriver.latest_driver_version.should eq(expected_semver)
-    end
-  end
-
-  describe ".driver_path" do
-    it "returns path to installed chromedriver" do
-      expected_path = File.expand_path("~/.webdrivers/chromedriver", home: Path.home)
-
-      Webdrivers::Chromedriver.driver_path.should eq(expected_path)
-    end
-  end
-
   describe ".remove" do
     it "deletes chromedriver" do
+      Webdrivers::Chromedriver.install
+
       File.exists?(Webdrivers::Chromedriver.driver_path).should be_true
       Webdrivers::Chromedriver.remove
       File.exists?(Webdrivers::Chromedriver.driver_path).should be_false
@@ -59,7 +12,7 @@ describe Webdrivers::Chromedriver do
   end
 
   describe ".install" do
-    it "installs the latest chromedriver" do
+    it "installs the chromedriver in the expected place" do
       Webdrivers::Chromedriver.remove
       driver_path = Webdrivers::Chromedriver.driver_path
 
@@ -72,6 +25,12 @@ describe Webdrivers::Chromedriver do
       expected_path = Webdrivers::Chromedriver.driver_path
 
       Webdrivers::Chromedriver.install.should eq(expected_path)
+    end
+
+    it "installs the latest chromedriver" do
+      Webdrivers::Chromedriver.install
+
+      Webdrivers::Chromedriver.driver_version.should eq(Webdrivers::Chromedriver.latest_driver_version)
     end
   end
 end

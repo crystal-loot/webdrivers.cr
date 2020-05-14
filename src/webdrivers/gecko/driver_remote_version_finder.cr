adf3f6c76967fb6ac2e9eff93840e486b409d8b0
Matthew McGarvey <tnraptor14@hotmail.com>
2020-05-13 19:24:41 -0500

    Remove halite from Webdrivers::Gecko::DriverRemoteVersionFinder

--- src/webdrivers/gecko/driver_remote_version_finder.cr
+++ src/webdrivers/gecko/driver_remote_version_finder.cr
@@ -11,13 +11,15 @@ class Webdrivers::Gecko::DriverRemoteVersionFinder
 
   private def find_raw_version
     Cache.fetch(cache_path, Webdrivers.settings.cache_duration) do
-      response = Halite.follow.get(
-        "https://github.com/mozilla/geckodriver/releases/latest",
-        headers: { "Accept" => "application/json" }
-      )
-      raise response.body if response.status_code >= 300
-      tag = response.parse["tag_name"]?.try &.as_s
-      tag.try &.lchop('v') # the tags are always like v1.0.0
+      HTTP::Client.get("https://github.com/mozilla/geckodriver/releases/latest") do |redirect_response|
+        redirect_location = redirect_response.headers["location"]
+        headers = HTTP::Headers{ "Accept" => "application/json" }
+        HTTP::Client.get(redirect_location, headers: headers) do |response|
+          raise response.body if response.status_code >= 300
+          tag = JSON.parse(response.body)["tag_name"]?.try &.as_s
+          tag.try &.lchop('v') # the tags are always like v1.0.0
+        end
+      end
     end
   end
 end

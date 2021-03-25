fa447b5a41eb82b2ecf3c26372803f2784201d3c
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2021-03-24 23:38:27 -0400

    v0.4.0

--- src/webdrivers.cr
+++ src/webdrivers.cr
@@ -9,7 +9,7 @@ require "semantic_version"
 require "./webdrivers/**"
 
 module Webdrivers
-  VERSION                  = "0.3.2"
+  VERSION                  = "0.4.0"
   DEFAULT_DRIVER_DIRECTORY = "~/.webdrivers"
 
   Habitat.create do

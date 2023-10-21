7f5b4196db561e8c99723e654d9e4f123b45b1a7
Jeremy Woertink <jeremywoertink@gmail.com>
2023-10-21 12:08:00 -0700

    Debugging windows CI because specs work locally on Windows, but fail in CI

--- src/webdrivers/common/zip_extractor.cr
+++ src/webdrivers/common/zip_extractor.cr
@@ -8,7 +8,12 @@ class Webdrivers::Common::ZipExtractor
 
   def extract
     Compress::Zip::File.open(zip_file) do |zip_contents|
-      driver = zip_contents[driver_name]
+      begin
+        driver = zip_contents[driver_name]
+      rescue
+        pp! zip_contents.entries
+        raise "Entry #{driver_name} not found in zip"
+      end
       filepath = driver.filename.split(File::SEPARATOR).last
       destination_path = File.join(install_path, filepath)
       File.delete(destination_path) if File.exists?(destination_path)

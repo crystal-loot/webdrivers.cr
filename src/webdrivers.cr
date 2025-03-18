3f3ea176e858d5392eb501d97d961d0e9e396621
Jeremy Woertink <jeremywoertink@gmail.com>
2025-03-18 14:02:08 -0700

    bump versions for next release (#21)

--- src/webdrivers.cr
+++ src/webdrivers.cr
@@ -9,7 +9,7 @@ require "semantic_version"
 require "./webdrivers/**"
 
 module Webdrivers
-  VERSION                  = "0.4.3"
+  VERSION                  = "0.4.4"
   DEFAULT_DRIVER_DIRECTORY = "~/.webdrivers"
 
   Habitat.create do
